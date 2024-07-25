b650973ea1eee8a9d20de99bf7f822ca9d0c6d4d
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-07-25 16:55:06 +0800

    added temporary api

---
+++ src/utils/sampleData.ts
@@ -0,0 +1,17 @@
+interface SampleData {
+	email: string;
+	password: string;
+}
+
+const sampleData: SampleData[] = [
+	{
+		email: "sample",
+		password: "123",
+	},
+	{
+		email: "admin",
+		password: "123",
+	},
+];
+
+export default sampleData;
