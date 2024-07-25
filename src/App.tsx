f25f30cea69b064d65b0cf116e6b54b97d259172
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-25 16:07:09 +0800

    Fixed Format

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
-	
 	return (
 		<BrowserRouter>
-			<Suspense fallback = {<div>Loading...</div>}>
+			<Suspense fallback={<div>Loading...</div>}>
 				<Routes>
 					<Route path="/" element={<div>hello</div>} />
 					<Route path="/login" element={<div>Login</div>} />
 					<Route path="/signup" element={<div>Signup</div>} />
 					<Route path="/forum" element={<div>Forum</div>} />
 					<Route path="/topic/:id" element={<div>Topics</div>} />
-				</Routes>	
+				</Routes>
 			</Suspense>
 		</BrowserRouter>
 	);
