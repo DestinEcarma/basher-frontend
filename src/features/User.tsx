7fc6ca3dc2b9f90063f4f6c61beca3d4f97f20ef
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-07-25 17:06:20 +0800

    Create User.tsx feature

---
+++ src/features/User.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface UserProps {
+	id: number;
+}
+
+const User: React.FC<UserProps> = ({ id }) => {
+	return (
+		<div className="flex flex-row items-center justify-center bg-accent p-1">
+			<h1>Anonymous #{id}</h1>
+		</div>
+	);
+}
+
+export default User;
