ccc8476107a304f5b6fa78bc9ab68f9e7356de12
Ron Patrick Ramas <143059096+MrChooey@users.noreply.github.com>
2024-07-25 18:27:57 +0800

    Update Topic.tsx
    Added: topic content

--- src/pages/Topic.tsx
+++ src/pages/Topic.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import User from "../features/User.tsx"
+
 const Topic = () => {
-	return <div></div>;
+	useEffect(() => {
+
+	}, []);
+
+	return (
+		<div>
+			<User id={0}/>
+		</div>
+	)
 };
 
 export default Topic;
