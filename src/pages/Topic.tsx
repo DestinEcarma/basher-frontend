7f3eea1cb03151922880d12f4e34d13de71e88c8
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-07-25 19:09:32 +0800

    added logo

--- src/pages/Topic.tsx
+++ src/pages/Topic.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import User from "../features/User.tsx";
+import Logo from "../components/Logo";
 
 const Topic = () => {
+	const { id } = useParams<{ id: string }>();
+
 	useEffect(() => {}, []);
 
 	return (
-		<div>
+		<div className="bg-[#F6F6F9] w-full min-h-screen">
+			<Logo />
+			<h1>Task # {id}</h1>
 			<User id={0} />
 		</div>
 	);
