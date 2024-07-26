a3fb0ee5ee9947df5ff57fd392df50258efd5c7a
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-07-26 12:49:53 +0800

    removed padding from Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo: React.FC = () => {
 	return (
-		<div className="flex font-bold items-center justify-center text-[6rem] leading-none pt-[2.688rem] tracking-wide">
+		<div className="flex font-bold items-center justify-center text-[6rem] leading-none tracking-wide">
 			<p className="flex items-center justify-center bg-black text-white size-[5.375rem] rounded-[0.625rem] leading-none">
 				B
 			</p>
