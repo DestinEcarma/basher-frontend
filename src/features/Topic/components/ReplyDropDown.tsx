7787639b7b8aa5d4118edaf5d734273513618e5a
Kwesten <christineanndejito21@gmail.com>
2024-07-28 20:55:31 +0800

    added formatting for dropdown

--- src/features/Topic/components/ReplyDropDown.tsx
+++ src/features/Topic/components/ReplyDropDown.tsx
@@ -8,10 +8,14 @@ interface ReplyDropdownProps {
 
 const ReplyDropdown: React.FC<ReplyDropdownProps> = ({ replies }) => {
 	return (
-		<div>
-			{replies.map((subreply) => (
-				<SubReply key={subreply.author} {...subreply} />
-			))}
+		<div className="flex flex-row items-stetch lg:max-w-7xl md:max-w-3xl w-full mt-3 mb-5">
+			<div className="border-l-4 border-gray-700 w-auto ml-10 mt-3 p-3">
+			</div>
+			<div>
+				{replies.map((subreply) => (
+					<SubReply key={subreply.author} {...subreply} />
+				))}
+			</div>
 		</div>
 	);
 };
