9824bfc3ca2539fe84a3c9760673496584fee36d
Dempsey <dagantareynat@gmail.com>
2024-07-30 22:30:48 +0800

    Fixed design

--- src/features/Forum/TopicTag.tsx
+++ src/features/Forum/TopicTag.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TopicTag = () => {
+const TopicTag: React.FC = () => {
 	return (
 		<div className="mb-1 mt-auto text-xs text-[#0096FB]">
 			<a href="#">#Tag</a>
