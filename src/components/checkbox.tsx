2e9a328b1bc1237ee9218b55c57ad9f81149c4bb
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-30 21:47:37 +0800

    Update checkbox.tsx

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,48 +1,37 @@
 import React from "react";
 import { FaCheck } from "react-icons/fa";
-import { ReactInputAttributes, ReactLabelAttributes } from "@utils/defs";
+import { ReactInputAttributes } from "@utils/defs";
 
 interface CheckBoxProps extends ReactInputAttributes {
 	label?: string;
 }
 
 const CheckBox = React.forwardRef<HTMLInputElement, CheckBoxProps>(({ label, name, ...props }, ref) => {
-	const [checked, setChecked] = React.useState(props.value ?? false);
+	const boxClassName = [
+		"relative h-5 w-5 cursor-pointer",
+		"rounded-md border border-gray-500",
+		"bg-white transition-colors",
+		"[&:has(input:checked)]:border-none",
+		"[&:has(input:checked)]:bg-blue-500",
+	];
 
-	const boxClassName: ReactLabelAttributes["className"] = [
-		"relative",
-		"h-5",
-		"w-5",
-		"cursor-pointer",
-		"rounded-md",
-		"border",
-		"border-gray-500",
-		"bg-white",
-		"transition-colors",
-		"data-[checked='true']:border-none",
-		"data-[checked='true']:bg-blue-500",
-	].join(" ");
-
-	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		props.onChange?.(e);
-		setChecked(e.target.checked);
-	};
+	const iconClassName = [
+		"absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
+		"text-sm text-white opacity-0 transition-opacity peer-checked:opacity-100",
+	];
 
 	return (
 		<div className="flex items-center gap-2 [&:has(label:hover)>label]:border-blue-500">
-			<label data-checked={checked} htmlFor={name} className={boxClassName}>
+			<label htmlFor={name} className={boxClassName.join(" ")}>
 				<input
 					type="checkbox"
 					id={name}
 					ref={ref}
 					name={name}
 					{...props}
-					onChange={onChange}
-					className="h-full w-full appearance-none"
+					className="peer h-full w-full appearance-none"
 				/>
-				{checked && (
-					<FaCheck className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-sm text-white" />
-				)}
+				<FaCheck className={iconClassName.join(" ")} />
 			</label>
 			{label && (
 				<label htmlFor={name} className="text-sm hover:cursor-pointer">
