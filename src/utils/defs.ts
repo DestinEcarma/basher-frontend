6faf4c68915cb92f7024e6555cc1be076a69ab62
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-30 10:51:13 +0800

    Update defs.ts

--- src/utils/defs.ts
+++ src/utils/defs.ts
@@ -5,3 +5,7 @@ export interface Children {
 export type ReactInputAttributes = React.InputHTMLAttributes<HTMLInputElement>;
 export type ReactLabelAttributes = React.LabelHTMLAttributes<HTMLLabelElement>;
 export type ReactButtonAttributes = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const EMAIL_REGEX =
+	// eslint-disable-next-line no-useless-escape
+	/(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
