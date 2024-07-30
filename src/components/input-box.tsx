f702adf5060afdfdb61ab5b0bb0f79e7689ac395
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-30 10:50:55 +0800

    Update input-box.tsx

--- src/components/input-box.tsx
+++ src/components/input-box.tsx
@@ -1,48 +1,48 @@
 import React from "react";
 import { LeftSide, RightSide } from "./sides";
-import { Children, ReactInputAttributes } from "@utils/defs";
+import { ReactInputAttributes } from "@utils/defs";
 
-interface InputBoxProps extends Children {
+interface InputBoxProps extends Omit<ReactInputAttributes, "type"> {
 	label?: string;
 	error?: string;
-	name?: ReactInputAttributes["name"];
-	value?: ReactInputAttributes["value"];
-	required?: ReactInputAttributes["required"];
 	type?: Exclude<ReactInputAttributes["type"], "radio" | "checkbox">;
-	onChange?: ReactInputAttributes["onChange"];
 }
 
-const InputBox: React.FC<InputBoxProps> = ({ error, name, label, children, ...props }) => {
-	const leftSide = [] as React.ReactNode[];
-	const rightSide = [] as React.ReactNode[];
+const InputBox = React.forwardRef<HTMLInputElement, InputBoxProps>(
+	({ error, name, label, children, ...props }, ref) => {
+		const leftSide = [] as React.ReactNode[];
+		const rightSide = [] as React.ReactNode[];
 
-	React.Children.forEach(children, (child) => {
-		if (!React.isValidElement(child)) {
-			return;
-		}
+		React.Children.forEach(children, (child) => {
+			if (!React.isValidElement(child)) {
+				return;
+			}
 
-		if (child.type === LeftSide) {
-			leftSide.push(child);
-		} else if (child.type === RightSide) {
-			rightSide.push(child);
-		}
-	});
+			if (child.type === LeftSide) {
+				leftSide.push(child);
+			} else if (child.type === RightSide) {
+				rightSide.push(child);
+			}
+		});
 
-	return (
-		<div className="flex flex-col gap-2">
-			{label && (
-				<label htmlFor={name} className="w-fit text-sm font-medium hover:cursor-pointer">
-					{label}
-				</label>
-			)}
-			<div className="flex items-center gap-2 rounded-lg border p-2 shadow transition-colors [&:has(input:focus)]:border-blue-500">
-				{leftSide}
-				<input id={name} name={name} placeholder={label} {...props} className="w-full text-sm" />
-				{rightSide}
+		console.log({ ...props });
+
+		return (
+			<div className="flex flex-col gap-2">
+				{label && (
+					<label htmlFor={name} className="w-fit text-sm font-medium hover:cursor-pointer">
+						{label}
+					</label>
+				)}
+				<div className="flex items-center gap-2 rounded-lg border p-2 shadow transition-colors [&:has(input:focus)]:border-blue-500">
+					{leftSide}
+					<input ref={ref} id={name} name={name} placeholder={label} {...props} className="w-full text-sm" />
+					{rightSide}
+				</div>
+				{error && <p className="text-xs text-red-500">{error}</p>}
 			</div>
-			{error && <p className="text-xs text-red-500">{error}</p>}
-		</div>
-	);
-};
+		);
+	},
+);
 
 export default InputBox;
