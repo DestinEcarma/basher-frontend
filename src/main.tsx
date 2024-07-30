eaa03e4f3d4bf8c19ce92d0874bb8fcfdf542619
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-31 04:39:48 +0800

    Update main.tsx
    Added: ApolloProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import React from "react";
+import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { client } from "@services/graphql.ts";
+import { ApolloProvider } from "@apollo/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<ApolloProvider client={client}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</ApolloProvider>,
 );
