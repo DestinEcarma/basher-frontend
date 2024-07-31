f95578f77aa814cec7f522d64f38604c23712918
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-08-01 05:41:24 +0800

    Update
    Fixed: apollo extension

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
 	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
-	ignorePatterns: ["dist", ".eslintrc.cjs", "*.svg"],
+	ignorePatterns: ["dist", ".eslintrc.cjs", "apollo.config.cjs", "*.svg"],
 	parser: "@typescript-eslint/parser",
 	plugins: ["react-refresh"],
 	rules: {
--- apollo.config.cjs
+++ apollo.config.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
 	client: {
 		service: {
 			name: "basher",
