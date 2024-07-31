a6c4190f9a572e5caeaa3e109d2ad88bba179f9d
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-08-01 05:52:25 +0800

    Create defs.ts

---
+++ src/features/sign-up/defs.ts
@@ -0,0 +1,17 @@
+import { gql } from "@apollo/client";
+
+export interface SignUpFields {
+	email: string;
+	password: string;
+	confirmPassword: string;
+}
+
+export const SIGN_UP = gql`
+	mutation SignUp($input: SignUpInput!) {
+		user {
+			signUp(input: $input)
+		}
+	}
+`;
+
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
