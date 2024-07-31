96eb09c7c785a2308f15e49e2c21d0715fcc49f5
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-08-01 05:43:51 +0800

    Create topic-skeleton.tsx

---
+++ src/features/Forum/components/topic-skeleton.tsx
@@ -0,0 +1,38 @@
+import { getRandomInt } from "@utils/helper";
+import clsx from "clsx";
+import React from "react";
+
+const TopicSkeleton: React.FC = () => {
+	const titleWidthClassName = ["w-5/6", "w-3/4", "w-2/3", "w-1/2", "w-1/3", "w-1/4", "w-1/5"];
+
+	return (
+		<tbody>
+			{new Array(10).fill(null).map(() => (
+				<tr className="border-b">
+					<td className="py-2">
+						<div className="flex flex-col gap-2">
+							<div
+								className={clsx([
+									"h-6 rounded-full bg-gray-300",
+									titleWidthClassName[getRandomInt(titleWidthClassName.length)],
+								])}
+							></div>
+							<div className="flex gap-1">
+								{new Array(getRandomInt(5)).fill(null).map(() => (
+									<div className="h-4 w-12 rounded-full odd:bg-gray-200 even:bg-gray-300"></div>
+								))}
+							</div>
+						</div>
+					</td>
+					{new Array(3).fill(null).map(() => (
+						<td className="group px-2">
+							<div className="mx-auto h-6 w-full rounded-full group-odd:bg-gray-300 group-even:bg-gray-200"></div>
+						</td>
+					))}
+				</tr>
+			))}
+		</tbody>
+	);
+};
+
+export default TopicSkeleton;
