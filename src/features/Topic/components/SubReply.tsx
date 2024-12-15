e6ab12f947ccb0d694bac6e1e549045bb1022f75
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-16 07:23:16 +0800

    Refactor: SubReply.tsx

--- src/features/Topic/components/SubReply.tsx
+++ src/features/Topic/components/SubReply.tsx
@@ -1,23 +1,24 @@
-import JumpToButton from "./JumpToButton";
+import { Reply } from "../utils/defs";
 import User from "./User";
 import MDEditor from "@uiw/react-md-editor";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-interface SubReplyProps {
-	id: number;
-	userIndex: number;
-	content: string;
-	timestamp: Date;
-}
+type SubReplyProps = Reply;
 
-const SubReply: React.FC<SubReplyProps> = ({ userIndex, content }) => {
+const SubReply: React.FC<SubReplyProps> = ({ id, userIndex, content }) => {
 	return (
-		<div className="subreply mt-3 flex w-full flex-col justify-center gap-3 rounded-md bg-white px-6 pb-5 pt-5 shadow-lg">
+		<div className="subreply mt-3 flex w-full flex-col gap-3 rounded-md bg-white px-6 pb-5 pt-5 shadow-lg">
 			<User index={userIndex} />
 			<div className="content flex w-full flex-col gap-5">
 				<MDEditor.Markdown source={content} />
 			</div>
-			<JumpToButton replyId="1">Jump to Reply</JumpToButton>
+			<NavLink
+				to={`#${id}`}
+				className="ml-auto select-none items-center gap-2 rounded-lg px-3 py-1 text-[#808080] duration-100 hover:cursor-pointer hover:bg-gray-100"
+			>
+				Jump to Reply
+			</NavLink>
 		</div>
 	);
 };
