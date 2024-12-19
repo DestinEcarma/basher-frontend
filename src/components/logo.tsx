a6f0f293877fd63f9a64df76a4f5324de056a27e
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-19 09:49:33 +0800

    Rename: Logo.tsx to logo.tsx

---
+++ src/components/logo.tsx
@@ -0,0 +1,47 @@
+import { ReactButtonAttributes } from "@utils/defs";
+import { mergeClasses } from "@utils/helper";
+import { cva, VariantProps } from "class-variance-authority";
+import { forwardRef } from "react";
+import { FaBold } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+
+export interface LogoProps extends ReactButtonAttributes, VariantProps<typeof variants> {}
+
+const variants = cva(null, {
+	variants: {
+		size: {
+			sm: "size-8 text-2xl",
+			lg: "size-11 text-4xl",
+			xl: "size-[4.25rem] text-6xl",
+		},
+	},
+});
+
+export const Logo = forwardRef<HTMLButtonElement, LogoProps>(({ size, className }, ref) => {
+	const navigate = useNavigate();
+
+	const onClick = () => {
+		navigate("/forum");
+	};
+
+	return (
+		<button
+			onClick={onClick}
+			ref={ref}
+			className={mergeClasses(
+				"flex flex-grow-0 select-none items-center font-roboto-mono text-2xl font-bold leading-none tracking-wide",
+				variants({ size, className }).replace(/size-\[.*?\]|data-\d+/g, ""),
+			)}
+		>
+			<span
+				className={mergeClasses(
+					"flex size-8 items-center justify-center rounded-lg bg-black text-2xl leading-none text-white",
+					variants({ size }),
+				)}
+			>
+				<FaBold />
+			</span>
+			<span>asher</span>
+		</button>
+	);
+});
