6363e9821e5fef7ce72c8f7fe6606d80b6f117d6
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-19 09:59:32 +0800

    Add: added auth and logout context

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,72 @@
-import Logo from "@components/Logo";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { AuthContext } from "@components/auth";
 import CreatePost from "@components/create-post";
+import { LogoutContext } from "@components/logout";
 import ForumPage from "@pages/forum";
 import LoginPage from "@pages/login";
 import SignUpPage from "@pages/sign-up";
 import TopicPage from "@pages/topic";
-import React, { Suspense } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { AUTH, AuthQuery, LOGOUT } from "@utils/defs";
+import React, { Suspense, useEffect, useState } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 
 const App: React.FC = () => {
+	const location = useLocation();
+
+	const [auth, setAuth] = useState(false);
+
+	const [lazyAuth] = useLazyQuery<AuthQuery>(AUTH, { fetchPolicy: "no-cache" });
+
+	const [logout] = useMutation(LOGOUT);
+
+	useEffect(() => {
+		(async () => {
+			const { data } = await lazyAuth();
+
+			if (data?.user.auth) {
+				setAuth(true);
+			}
+		})();
+	}, [lazyAuth, location.pathname]);
+
+	const onLogout = () => {
+		logout();
+		setAuth(false);
+		sessionStorage.removeItem("connect.sid");
+	};
+
 	return (
 		<div className="flex h-dvh flex-col">
 			<Toaster richColors duration={5000} closeButton position="top-right" />
-			<BrowserRouter>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Logo />
-					<Routes>
-						<Route path="/" element={<Navigate to="/forum" />} />
-						<Route path="/login" element={<LoginPage />} />
-						<Route path="/sign-up" element={<SignUpPage />} />
-						<Route
-							path="/forum"
-							element={
-								<CreatePost.Display>
-									<ForumPage />
-								</CreatePost.Display>
-							}
-						/>
-						<Route
-							path="/topic/:id"
-							element={
-								<CreatePost.Display>
-									<TopicPage />
-								</CreatePost.Display>
-							}
-						/>
-					</Routes>
-				</Suspense>
-			</BrowserRouter>
+			<Suspense fallback={<div>Loading...</div>}>
+				<AuthContext.Provider value={auth}>
+					<LogoutContext.Provider value={onLogout}>
+						<Routes>
+							<Route path="/" element={<Navigate to="/forum" />} />
+							<Route path="/login" element={<LoginPage />} />
+							<Route path="/sign-up" element={<SignUpPage />} />
+							<Route
+								path="/forum"
+								element={
+									<CreatePost.Display>
+										<ForumPage />
+									</CreatePost.Display>
+								}
+							/>
+							<Route
+								path="/topic/:id"
+								element={
+									<CreatePost.Display>
+										<TopicPage />
+									</CreatePost.Display>
+								}
+							/>
+							<Route path="*" element={<Navigate to="/forum" />} />
+						</Routes>
+					</LogoutContext.Provider>
+				</AuthContext.Provider>
+			</Suspense>
 		</div>
 	);
 };
