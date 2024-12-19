d72ccc54713770cc08ed4dc376f6ee47c01f1a8a
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-19 09:56:01 +0800

    Add: added BrowserRouter to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { ApolloProvider } from "@apollo/client";
 import { client } from "@services/graphql.ts";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 document.documentElement.setAttribute("data-color-mode", "light");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<ApolloProvider client={client}>
 		<React.StrictMode>
-			<App />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
 		</React.StrictMode>
 	</ApolloProvider>,
 );
