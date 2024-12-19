2976320db253e9022c9a4f8439375d4557efa55a
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-12-19 09:55:20 +0800

    Add, Refactor: added, authorization header and refactor each items from link field

--- src/services/graphql.ts
+++ src/services/graphql.ts
@@ -1,4 +1,5 @@
 import { ApolloClient, from, HttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { onError } from "@apollo/client/link/error";
 import { GraphQLFormattedError } from "graphql";
 import { toast } from "sonner";
@@ -22,39 +23,52 @@ export enum GraphqlErrorType {
 
 	// Authorization errors
 	UNAUTHORIZED = "UNAUTHORIZED",
+
 	INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR",
 }
+
+const authLink = setContext((_, { headers }) => {
+	const token = sessionStorage.getItem("connect.sid");
+
+	return {
+		headers: {
+			...headers,
+			Authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});
+
+const errorHandler = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors) {
+		graphQLErrors.map(({ message }: GraphqlError) => {
+			console.log(message);
+			switch (message) {
+				case GraphqlErrorType.BAD_REQUEST:
+					toast.error("An error occurred: Bad Request");
+					break;
+				case GraphqlErrorType.INTERNAL_SERVER_ERROR:
+					toast.error("An error occurred: Internal Server Error");
+					break;
+				default:
+					if (Object.values(GraphqlErrorType).includes(message as GraphqlErrorType)) {
+						break;
+					}
+
+					toast.error("An error occurred: Unknown Error");
+					break;
+			}
+		});
+	}
+
+	if (networkError) {
+		toast.error("An error occurred: Network Error");
+	}
+});
+
+const httpLink = new HttpLink({ uri: "/graphql" });
 
 export const client = new ApolloClient({
 	cache: new InMemoryCache(),
 	credentials: "include",
-	link: from([
-		onError(({ graphQLErrors, networkError }) => {
-			if (graphQLErrors) {
-				graphQLErrors.map(({ message }: GraphqlError) => {
-					switch (message) {
-						case GraphqlErrorType.BAD_REQUEST:
-							toast.error("An error occurred: Bad Request");
-							break;
-						case GraphqlErrorType.INTERNAL_SERVER_ERROR:
-							toast.error("An error occurred: Internal Server Error");
-							break;
-						default:
-							if (Object.values(GraphqlErrorType).includes(message as GraphqlErrorType)) {
-								break;
-							}
-
-							toast.error("An error occurred: Unknown Error");
-							break;
-					}
-				});
-			}
-
-			if (networkError) {
-				toast.error("An error occurred: Network Error");
-			}
-		}),
-
-		new HttpLink({ uri: "/graphql" }),
-	]),
+	link: from([authLink, errorHandler, httpLink]),
 });
